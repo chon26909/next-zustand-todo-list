8d6ea64df833073b1bda7daefbcef38453a6e462
HiChon <59905927+chon26909@users.noreply.github.com>
2024-10-18 00:03:01 +0700

    column

--- components/Column.tsx
+++ components/Column.tsx
@@ -1,28 +1,50 @@
-import React from 'react';
-import Task from './Task';
+'use client';
 
-const tasks = [
-    {
-        id: '1234',
-        title: 'Task 1',
-        description: 'some description',
-        status: 'TODO'
-    }
-];
+import { Status, useTaskStore } from '@/store/task';
+import Task from './task';
+import { useMemo, DragEvent, useEffect } from 'react';
 
-type ColumnProps = {
+export default function Column({
+    title,
+    status
+}: {
     title: string;
-    status: string;
-};
+    status: Status;
+}) {
+    const tasks = useTaskStore((state) => state.tasks);
+    const filteredTasks = useMemo(
+        () => tasks.filter((task) => task.status === status),
+        [tasks, status]
+    );
 
-const Column = (props: ColumnProps) => {
-    const filteredTasks = tasks.filter((task) => task.status === props.status);
+    const updateTask = useTaskStore((state) => state.updateTask);
+    const draggedTask = useTaskStore((state) => state.draggedTask);
+    const dragTask = useTaskStore((state) => state.dragTask);
+
+    const handleDrop = (e: DragEvent<HTMLElement>) => {
+        if (draggedTask) {
+            updateTask(draggedTask, status);
+            dragTask(null);
+        }
+    };
+
+    useEffect(() => {
+        const restore = localStorage.getItem('restore') === 'true';
+        console.log('restore', restore);
+        if (restore) {
+            useTaskStore.persist.rehydrate();
+        }
+    }, []);
 
     return (
-        <section className='h-[600px] flex-1'>
-            <h2 className='ml-1 font-serif text-2xl font-semibold'>{props.title}</h2>
+        <section className='h-[500px] flex-1'>
+            <h2 className='ml-1 font-serif text-2xl font-semibold'>{title}</h2>
 
-            <div className='mt-3.5 h-full w-full flex-1 rounded-xl bg-gray-700/50 p-4'>
+            <div
+                className='mt-3.5 h-full w-full flex-1 rounded-xl bg-gray-700/50 p-4'
+                onDrop={handleDrop}
+                onDragOver={(e) => e.preventDefault()}
+            >
                 <div className='flex flex-col gap-4'>
                     {filteredTasks.map((task) => (
                         <Task key={task.id} {...task} />
@@ -31,6 +53,4 @@ const Column = (props: ColumnProps) => {
             </div>
         </section>
     );
-};
-
-export default Column;
+}
