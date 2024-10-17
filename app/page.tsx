5eee4ea57b471be9afa9e97b69ca08b420715427
HiChon <59905927+chon26909@users.noreply.github.com>
2024-10-18 00:02:52 +0700

    home

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,11 @@
-import Columns from '@/components/Columns';
-import React from 'react';
+import Columns from '@/components/columns'
 
-const HomePage = () => {
-    return (
-        <section className='flex h-screen bg-gradient-to-bl from-gray-700'>
-            <div className='mx-auto w-full max-w-7xl px-6'>
-                <Columns />
-            </div>
-        </section>
-    );
-};
-
-export default HomePage;
+export default function Home() {
+  return (
+    <section className='flex h-screen bg-gradient-to-br from-gray-700 to-gray-900 py-12 text-white'>
+      <div className='mx-auto w-full max-w-7xl px-6'>
+        <Columns />
+      </div>
+    </section>
+  )
+}
