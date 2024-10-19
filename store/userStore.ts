ed20904d9b9b8dfcf9eb4a1e692876ae02e0f04f
HiChon <59905927+chon26909@users.noreply.github.com>
2024-10-19 23:48:55 +0700

    user store

---
+++ store/userStore.ts
@@ -0,0 +1,12 @@
+import { GetUsersFilters } from '@/api/user';
+import { create } from 'zustand';
+
+type UserStore = {
+    filters?: GetUsersFilters;
+    setFilters: (filters?: GetUsersFilters) => void;
+};
+
+export const useUserStore = create<UserStore>((set) => ({
+    filters: undefined,
+    setFilters: (filters) => set({ filters })
+}));
